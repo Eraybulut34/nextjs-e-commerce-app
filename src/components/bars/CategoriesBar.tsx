e5deb27f2b3ad898a5f65ad01b566518ea0a7150
eraybulut34 <abdullaheraybulut@hotmail.com>
2023-03-27 14:22:07 +0300

    categories bar responsive

--- src/components/bars/CategoriesBar.tsx
+++ src/components/bars/CategoriesBar.tsx
@@ -1,34 +1,60 @@
-import React from "react";
-import { Col, Row, Card } from "antd";
+import React, { useEffect, useState } from "react";
+import { Col, Row, Card, Menu } from "antd";
 import { fakeCategories } from "../../../fakeCategories";
+import type { MenuProps } from 'antd';
+import { Button, Dropdown } from 'antd';
+import { useWindowSize } from "@/hooks/index";
 
-const styles = {
-    cardStyle: {
-        boxSizing: "border-box",
-        borderRadius: "0px",
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        padding: "0px",
-        height: "4rem",
-        overflow: "auto",
-        boxShadow: "0 1px 2px 0 rgba(0,0,0,0.2), 0 3px 5px 0 rgba(0,0,0,0.19)"
-    } as React.CSSProperties,
-};
 
 function FeaturedProducts() {
+    const [innerWidth, setInnerWidth] = useState(0);
+    const styles = {
+        cardStyle: {
+            boxSizing: "border-box",
+            borderRadius: "0px",
+            display: "flex",
+            justifyContent: innerWidth < 1200 ? "flex-start" : "center",
+            alignItems: "center",
+            padding: "0px",
+            height: "4rem",
+            overflow: "auto",
+            boxShadow: "0 1px 2px 0 rgba(0,0,0,0.2), 0 3px 5px 0 rgba(0,0,0,0.19)"
+        } as React.CSSProperties,
+    };
+    useEffect(() => {
+        setInnerWidth(window.innerWidth);
+    }, [useWindowSize()]);
     return (
         <Card style={styles.cardStyle}>
             <Row>
-                {fakeCategories.map((content: any) => (
-                    <Col xl={2}
-                        lg={4}
-                        xs={22}
-                    
-                    style={{ marginInline: "1rem" }} key={content.title}>
-                        {content.title}
-                    </Col>
-                ))}
+                {
+                    innerWidth > 1200 ? (
+                        fakeCategories.map((content: any) => (
+                            <Col xl={2}
+                                lg={4}
+                                xs={22}
+
+                                style={{ marginInline: "1rem" }} key={content.title}>
+                                {content.title}
+                            </Col>
+                        ))) : (
+                        <Dropdown overlay={
+                            <Menu>
+                                {
+                                    fakeCategories.map((content: any) => (
+                                        <Menu.Item key={content.title}>
+                                            {content.title}
+                                        </Menu.Item>
+                                    ))
+                                }
+                            </Menu>
+                        }>
+                            <Button>Kategoriler</Button>
+                        </Dropdown>
+
+
+                    )
+                }
             </Row>
         </Card>
     );
